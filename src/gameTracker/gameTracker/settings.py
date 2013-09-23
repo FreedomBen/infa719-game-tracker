fac439fdf33d8a5d4796b1f877a7b4efe38e608b
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-09-22 20:51:52 -0600

    settings.py: add template directory location

--- src/gameTracker/gameTracker/settings.py
+++ src/gameTracker/gameTracker/settings.py
@@ -111,10 +111,10 @@ ROOT_URLCONF = 'gameTracker.urls'
 WSGI_APPLICATION = 'gameTracker.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+     join( PROJECT_DIR, 'templates/' ), 
 )
+
+print( join( PROJECT_DIR, 'templates/' ) )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
