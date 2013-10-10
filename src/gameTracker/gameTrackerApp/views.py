4cbe9a2b1e9952d8e69357abc5ed9e3645d741b8
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-10-09 18:50:33 -0600

    Add stubbed definition for login and register pages

--- src/gameTracker/gameTrackerApp/views.py
+++ src/gameTracker/gameTrackerApp/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+# main definition of view code
+
+from django.http import HttpResponse
+
+def login( request ):
+    return HttpResponse( "login" )
+
+def register( request ):
+    return HttpResponse( "register" )
