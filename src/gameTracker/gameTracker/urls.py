ae85304714c12206967c906293f06c835c5492f5
FreedomBen <bb1netusaf2004@hotmail.com>
2013-10-21 12:47:13 -0600

    Update urls.py
    Add default for testing

--- src/gameTracker/gameTracker/urls.py
+++ src/gameTracker/gameTracker/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns( '',
     url( r'^login/', 'gameTrackerApp.views.login' ),
     url( r'^register/', 'gameTrackerApp.views.register' ),
+    url( r'^default/', 'gameTrackerApp.views.default' ),
     url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
     url( r'^admin/', include( admin.site.urls ) ),
 )
