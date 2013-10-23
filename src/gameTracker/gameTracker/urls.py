2afd73d6122e2d52262aab84200ea48b964a08c3
Benjamin Porter <benjamin.porter@example.com>
2013-10-22 21:18:11 -0600

    urls.py add $ to regexes

--- src/gameTracker/gameTracker/urls.py
+++ src/gameTracker/gameTracker/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns( '',
-    url( r'^login/', 'gameTrackerApp.views.login' ),
-    url( r'^register/createuser', 'gameTrackerApp.views.register_user' ),
-    url( r'^register/', 'gameTrackerApp.views.register' ),
-    url( r'^default/', 'gameTrackerApp.views.default' ),
+    url( r'^login/$', 'gameTrackerApp.views.login' ),
+    url( r'^register/createuser/$', 'gameTrackerApp.views.register_user' ),
+    url( r'^register/$', 'gameTrackerApp.views.register' ),
+    url( r'^default/$', 'gameTrackerApp.views.default' ),
     url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
     url( r'^admin/', include( admin.site.urls ) ),
 )
