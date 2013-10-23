077f52859727b35f9560a0e3461a5bbb65f477ad
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-10-22 19:03:44 -0600

    views.py add comments and stub register_user page

--- src/gameTracker/gameTrackerApp/views.py
+++ src/gameTracker/gameTrackerApp/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+# This view serves the login page
 def login( request ):
     return render_to_response( 'login.html', { } )
 
+# This view serves the register page
 def register( request ):
     return render_to_response( "register.html", { }, context_instance=RequestContext( request ) )
 
+# This view processes the registration request
+def register_user( request ):
+    # validate our data server side before processing it
+    pass
+
+# This view is for testing the default template
 def default( request ):
     return render_to_response( "default.html", { } )
