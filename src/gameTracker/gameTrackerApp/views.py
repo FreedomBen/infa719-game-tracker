62400f8915118b41159b96ef7b0a26ce56780403
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-10-22 19:00:20 -0600

    Allow the view to read the CSRF token from the submitting page

--- src/gameTracker/gameTrackerApp/views.py
+++ src/gameTracker/gameTrackerApp/views.py
@@ -2,12 +2,13 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def login( request ):
     return render_to_response( 'login.html', { } )
 
 def register( request ):
-    return render_to_response( "register.html", { } )
+    return render_to_response( "register.html", { }, context_instance=RequestContext( request ) )
 
 def default( request ):
     return render_to_response( "default.html", { } )
