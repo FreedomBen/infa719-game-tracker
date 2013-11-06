1993a4cd6f41b3b4215d54ce66ee1221187c6b4b
Zach Anderson <zlanderson@pluto.dsu.edu>
2013-11-05 19:23:57 -0600

    added logout functionality and return to login from register

--- src/gameTracker/gameTracker/urls.py
+++ src/gameTracker/gameTracker/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns( '',
     url( r'^create/$', 'gameTrackerApp.views.create' ),
     url( r'^join/$', 'gameTrackerApp.views.join' ),
     url( r'^view/$', 'gameTrackerApp.views.view' ),
+	url( r'^logoutView/$', 'gameTrackerApp.views.logoutView' ),
     url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
     url( r'^admin/', include( admin.site.urls ) ),
 ) + static( settings.STATIC_URL, document_root=settings.STATIC_ROOT )
